e9dcf01895e7148b8c57909b0937d3deb8d2784a
destr_2 <modv081214hgtrmca5@cbtis217.edu.mx>
2024-10-14 08:03:24 -0600

    se puso el codigo

---
+++ distancia-dos-puntos.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+
+double x1, x2, y1, y2, d;
+  cout<<"Digite los numeros"<<endl;
+  cout<<"digite x1"<<endl;
+  cin>>x1;
+  cout<<"digite y1"<<endl;
+  cin>>y1;
+  cout<<"digite x2"<<endl;
+  cin>>x2;
+  cout<<"digite y2"<<endl;
+  cin>>y2;
+  d=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+  cout<<endl;
+  cout<<"la distancia entre los dos puntos es: ";
+  cout<<d;
+
+
+return 0;}
+
